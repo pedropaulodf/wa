cfaa9e8f8610cb785b426f308db3d41fea7f147b
Pedro Paulo <pedropaulotj@gmail.com>
2024-02-21 13:53:55 -0300

    fix: Adição botão Web

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-const iptTelefone = document.getElementById('telefone');
+const iptTelefone = document.getElementById("telefone");
 
-function abrirWa(){
-  if(iptTelefone.value.length < 1){
-    alert('Campo telefone em branco!');
+function abrirWa() {
+  if (iptTelefone.value.length < 1) {
+    alert("Campo telefone em branco!");
     return;
   }
-  if(limpaCaracteresEspacosTelefone(iptTelefone.value).length !== 11){
-    alert('Telefone faltando número!');
-    return;
-  }
-  window.open(`https://wa.me/+55${limpaCaracteresEspacosTelefone(iptTelefone.value)}`);
+  // if (limpaCaracteresEspacosTelefone(iptTelefone.value).length !== 11) {
+  //   alert("Telefone faltando número!");
+  //   return;
+  // }
+  window.open(
+    `https://wa.me/+55${limpaCaracteresEspacosTelefone(iptTelefone.value)}`
+  );
 }
 
-function limpaCaracteresEspacosTelefone(tel){
-  return tel.replace(/[^A-Z0-9]/ig, "");
+function abrirWaWeb() {
+  if (iptTelefone.value.length < 1) {
+    alert("Campo telefone em branco!");
+    return;
+  }
+  // if (limpaCaracteresEspacosTelefone(iptTelefone.value).length !== 11) {
+  //   alert("Telefone faltando número!");
+  //   return;
+  // }
+  window.open(
+    `https://web.whatsapp.com/send/?phone=+55${limpaCaracteresEspacosTelefone(
+      iptTelefone.value
+    )}&text&type=phone_number&app_absent=0`
+  );
+}
+function limpaCaracteresEspacosTelefone(tel) {
+  return tel.replace(/[^A-Z0-9]/gi, "");
 }
