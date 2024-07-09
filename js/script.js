9bdd0954455122dd316b84f68e916eef493b2e6e
pedropaulodf <pedropaulotj@gmail.com>
2024-07-09 17:42:00 -0300

    feat: Focus input e tecla enter

--- js/script.js
+++ js/script.js
@@ -33,3 +33,20 @@ function abrirWaWeb() {
 function limpaCaracteresEspacosTelefone(tel) {
   return tel.replace(/[^A-Z0-9]/gi, "");
 }
+
+// Focus no input
+document.getElementById("telefone").focus();
+
+// Quando apertar enter
+let input = document.getElementById("telefone");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btnweb").click();
+  }
+});
